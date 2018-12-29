7fd7a0a9d3811da8c1e5b0709d49594030ec23c2
hugodu69 <hugogogo@protonmail.com>
2018-12-29 18:03:52 +0100

    debuggin invisibles mp4

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	$(".diaporama").diaporama({
+		animationSpeed: 1500,
+		delay:1.5
+	});
+
+});
---
+++ javascript/tools.js
@@ -0,0 +1,120 @@
+var coordsArtiste=new Array();
+coordsArtiste[0]="<span style='font-weight:bold;font-size:11px;'>Malgorzata (Margot) Montenoise</span><br /><a href='http://montenoisemargot.ultra-book.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:10px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>montenoisemargot.ultra-book.com</a><br /><a href='mailto:margot.montenoise@gmail.com' style='color:#cc3300;text-decoration:none;font-size:10px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>margot.montenoise@gmail.com</a>";
+coordsArtiste[1]="<span style='font-weight:bold;'>Christophe Bogdan</span><br /><a href='mailto:kbogdan@wanadoo.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>kbogdan@wanadoo.fr</a>";
+coordsArtiste[2]="<span style='font-weight:bold;'>Macha Krivokapic</span><br /><a href='mailto:machak91@yahoo.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>machak91@yahoo.fr</a>";
+coordsArtiste[3]="<span style='font-weight:bold;'>Vincent Pandellé</span><br /><a href='http://www.passionphotographique.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>passionphotographique.com</a><br /><a href='mailto:pandelle@yahoo.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>pandelle@yahoo.fr</a>";
+coordsArtiste[4]="<span style='font-weight:bold;'>Jean José Baranes</span><br /><a href='http://www.jeanjosebaranes.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>jeanjosebaranes.com</a><br /><a href='mailto:jeanjose.baranes@orange.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>jeanjose.baranes@orange.fr</a>";
+coordsArtiste[5]="<span style='font-weight:bold;'>Eliza Magri</span><br /><a href='http://www.elizamagri.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.elizamagri.com</a>";
+coordsArtiste[6]="<span style='font-weight:bold;'>Isabelle Rince</span><br /><a href='http://www.peintre-rince.odexpo.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.peintre-rince.odexpo.com</a><br /><a href='mailto:isrince@gmail.com' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>isrince@gmail.com</a>";
+coordsArtiste[7]="<span style='font-weight:bold;'>Jérôme Bouchez</span><br /><a href='http://www.jerome-bouchez.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.jerome-bouchez.com</a>";
+coordsArtiste[8]="<span style='font-weight:bold;'>Paule Millara</span><br /><a href='mailto:paule.millara@orange.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>paule.millara@orange.fr</a>";
+coordsArtiste[9]="<span style='font-weight:bold;'>Dashan Yang</span><br /><a href='mailto:dashan.2a@gmail.com' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>dashan.2a@gmail.com</a>";
+coordsArtiste[10]="<span style='font-weight:bold;'>Altone Mishino</span><br /><a href='http://www.mishino.artists.de' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.mishino.artists.de</a><br /><a href='http://www.mishino.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.mishino.com</a><br /><a href='http://www.artmajeur.com/mishinonews' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.artmajeur.com/mishinonews</a><br><a href='http://www.kazoart.com/artiste-contemporain/26-altone-mishino' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.kazoart.com</a><br><a href='http://www.saatchiart.com/altone.mishino' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.saatchiart.com</a>";
+coordsArtiste[11]="<span style='font-weight:bold;'>Macha Pandellé</span><br /><a href='http://grafima.ucoz.net/' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>grafima.ucoz.net</a>";
+coordsArtiste[12]="<span style='font-weight:bold;'>Milan Atanaskovic</span><br /><a href='http://www.oart.tv' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.oart.tv</a><br /><a href='mailto:milan@artchannel.info' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>milan@artchannel.info</a>";
+coordsArtiste[13]="<span style='font-weight:bold;'>Lahouari Mansouri dit Wari</span><br /><a href='mailto:wariraiband@wanadoo.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>wariraiband@wanadoo.fr</a>";
+coordsArtiste[14]="<span style='font-weight:bold;'>Anne Mauban, alias Anna</span><br /><a href='mailto:bernard.mauban@orange.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>bernard.mauban@orange.fr</a>";
+coordsArtiste[15]="<span style='font-weight:bold;'>Mitou Alalinarde</span><br /><a href='mailto:a.mitou@orange.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>a.mitou@orange.fr</a>";
+coordsArtiste[16]="<span style='font-weight:bold;'>Nicolas de Ferran</span><br /><a href='http://www.facebook.com/pages/Le-Tiroir-Jaune/104858274036' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>Facebook : Le tiroir jaune</a><br /><a href='mailto:nicolas.def@gmail.com' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>nicolas.def@gmail.com</a>";
+coordsArtiste[17]="<span style='font-weight:bold;'>Claudine Sabatier</span><br /><a href='http://lessabatier.fr/claudine' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>lessabatier.fr/claudine/</a><br /><a href='mailto:claudine.lessabatier@orange.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>claudine.lessabatier@orange.fr</a>";
+coordsArtiste[18]="<span style='font-weight:bold;'><span style='color:#cc3300;'>Cliquez</span> sur la photo de</span><br /><span style='font-weight:bold;'>votre choix pour afficher </span><br /><span style='font-weight:bold;'>les informations.</span>";
+coordsArtiste[19]="<span style='font-weight:bold;'>Arnaud de l'Estourbeillion</span><br /><a href='mailto:arnauddelestourbeillon@wanadoo.fr' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>arnauddelestourbeillon<br />@wanadoo.fr</a>";
+coordsArtiste[20]="<span style='font-weight:bold;'>François Gibault</span><br /><a href='http://fgibault.free.fr' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>fgibault.free.fr</a>";
+coordsArtiste[21]="<span style='font-weight:bold;'>Françoise Delecroix</span><br /><a href='http://www.francoise-delecroix.fr' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>www.francoise-delecroix.fr</a><br /><a href='mailto:francoise.delecroix@gmail.com' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>francoise.delecroix@gmail.com</a>";
+/*
+ajouter un nouvel artiste
+
+attention coordsArtiste[], mettre dans les crochet le n° qui suit le dernier du tableau existant ci-dessus
+
+à ajouter au tableau :
+
+coordsArtiste[...]="<span style='font-weight:bold;'>... Nom de l'artiste</span><br /><a href='http://www.chemin du site de l'artiste' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>... nom du site de l'artiste</a><br /><a href='mailto:... adresse mail de l'artiste' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>Adresse mail de l'artiste</a>";
+
+s'il n'y a pas de site web on supprime 
+"<a href='http://www.chemin du site de l'artiste' target='_blank' style='color:#cc3300;text-decoration:none;font-size:11px;' onmouseover='this.style.color=\"#000000\"' onmouseout='this.style.color=\"#cc3300\"'>... nom du site de l'artiste</a><br />"
+
+sortir coordsArtiste[]=" ... </a>"; 
+et coller derrière le tableau ci-dessus
+*/
+
+function init(){
+document.getElementById("affichage").style.visibility="visible";
+document.getElementById("affichage").innerHTML=coordsArtiste[18];
+}
+
+function afficher(obj){
+document.getElementById("affichage").style.visibility="visible";
+document.getElementById("affichage").innerHTML=coordsArtiste[obj];
+}
+
+/* ci-après pour l'image à l'ouverture de la page */
+
+function initSurvol(obj){
+document.getElementById("survol").src="images/interstices/interstices4.jpg";
+}
+
+
+/* page interstices */
+/* pour ajouter une image, calculer une image soit sur une hauteur de 520px (pour les images hautes) -ou- une largeur de 500px (pour les images larges) et la centrer sur un fond noir de 520 px x 500 px, optimiser en jpg pour le web et l'enrégistrer au nom de interstices9.jpg, interstices10.jpg et ainsi de suite*/
+
+/* ajouter à la liste ci-dessous la ligne
+monTableau[8] = "images/interstices/interstices9.jpg";
+monTableau[9] = "images/interstices/interstices10.jpg";
+et ainsi de suite */
+
+
+var monTableau= new Array();
+monTableau[0] = "images/interstices/interstices1.jpg";
+monTableau[1] = "images/interstices/interstices2.jpg";
+monTableau[2] = "images/interstices/interstices3.jpg";
+monTableau[3] = "images/interstices/interstices4.jpg";
+monTableau[4] = "images/interstices/interstices5.jpg";
+monTableau[5] = "images/interstices/interstices6.jpg";
+monTableau[6] = "images/interstices/interstices7.jpg";
+monTableau[7] = "images/interstices/interstices8.jpg";
+
+function text_over(obj){
+
+document.getElementById("survol").src=monTableau[obj];
+}
+
+
+/* page partenaires */
+/* les initiés comprendront */
+
+var monPartenaire=new Array();
+monPartenaire[0] = "images/logoSceauxNoir.png";
+monPartenaire[1] = "images/logoSceauxNoir_over.png";
+monPartenaire[2] = "images/oArt.png";
+monPartenaire[3] = "images/oArt_over.png";
+
+function initPart(){
+document.getElementById("p1").style.visibility="visible";
+document.getElementById("p1").src="images/logoSceauxNoir.png";
+}
+
+function initPart1(){
+document.getElementById("p2").style.visibility="visible";
+document.getElementById("p2").src="images/oArt.png";
+}
+
+function showLogo(){
+	document.getElementById("p1").style.visibility="visible";
+	document.getElementById("p1").src="images/logoSceauxNoir_over.png";
+	
+	}
+	
+function showLogo1(obj){
+	document.getElementById("p2").style.visibility="visible";
+	document.getElementById("p2").src="images/oArt_over.png";
+	
+	}
+
+
+
+
+
+
+
+
+
